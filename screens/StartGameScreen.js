59e8959dd37dfa8b7ea778499959c44fd55fc05a
Brad Stricherz <Brad.Stricherz@gmail.com>
2024-05-04 19:55:21 -0500

    Refactor code structure and update dependencies
    Changes include replacing App.js and GoalItem.js with other components and screens, adding babel.config.js, StartGameScreen.js and package-lock.json. Furthermore, dependencies in package.json were updated along with import changes in the App.js.

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,81 @@
+import {View, TextInput, StyleSheet, Alert} from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+import {useState} from "react";
+
+function StartGameScreen({onPickNumber}) {
+
+    const [enteredNumber, setEnteredNumber] = useState('');
+
+    function numberInputHandler(enteredText) {
+        setEnteredNumber(enteredText);
+    }
+
+    function confirmInputHandler() {
+        const chosenNumber = parseInt(enteredNumber);
+        if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+            Alert.alert('Invalid Number!', 'Number has to be a number between 1 and 99.', [{
+                text: 'Okay',
+                style: 'destructive',
+                onPress: resetInputHandler,
+            }]);
+            return;
+        }
+        onPickNumber(chosenNumber);
+    }
+
+    function resetInputHandler() {
+        setEnteredNumber('');
+    }
+
+    return (
+        <View style={styles.inputContainer}>
+            <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" autoCapitalize="none"
+                       autoCorrect={false} value={enteredNumber} onChangeText={numberInputHandler}/>
+            <View style={styles.buttonsContainer}>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+                </View>
+            </View>
+        </View>
+    );
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+        marginHorizontal: 24,
+        padding: 16,
+        marginTop: 100,
+        backgroundColor: "#3b021f",
+        borderRadius: 8,
+        elevation: 4,
+        shadowColor: "black",
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: "#DDB52F",
+        borderBottomWidth: 2,
+        color: "#DDB52F",
+        marginVertical: 8,
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+    buttonsContainer: {
+        flexDirection: "row",
+    },
+    buttonContainer: {
+        flex: 1,
+    },
+});
